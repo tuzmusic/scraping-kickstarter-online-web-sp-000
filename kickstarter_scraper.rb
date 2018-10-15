4d63ac5a3f9174a2a66f6f6fffba6f030fd87750
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-15 14:57:37 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -16,7 +16,7 @@ def create_project_hash
   }
   projects
   # for each project:
-  title: project.css('h2.bbcard_name strong a').text
+  # title: project.css('h2.bbcard_name strong a').text
   # image link: project.css('div.project-thumbnail a img').attribute('src').value
   # description: project.css('p.bbcard_blurb').text
   # location: project.css('ul.project-meta span.location-name').first.text
