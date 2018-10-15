617b8061975aa129121c104d72ef589253d535c2
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-15 14:54:40 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,16 +6,22 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   projects = {}
   kickstarter.css('li.project.grid_4').each { |project|
-    projects[project] = {}
+    title = project.css('h2.bbcard_name strong a').text
+    projects[title.to_sym] = {
+      image-link: project.css('div.project-thumbnail a img').attribute('src').value
+      description: project.css('p.bbcard_blurb').text
+      location: project.css('ul.project-meta span.location-name').first.text
+      percent-funded: project.css('ul.project-stats li').first.css('strong').text.chomp('%').to_i
+    }
   }
   projects
   # for each project:
-  # title: project.css('h2.bbcard_name strong a').text
+  title: project.css('h2.bbcard_name strong a').text
   # image link: project.css('div.project-thumbnail a img').attribute('src').value
   # description: project.css('p.bbcard_blurb').text
   # location: project.css('ul.project-meta span.location-name').first.text
   # percent funded: project.css('ul.project-stats li').first.css('strong').text.chomp('%').to_i
-  binding.pry
+
 end
 
 create_project_hash
