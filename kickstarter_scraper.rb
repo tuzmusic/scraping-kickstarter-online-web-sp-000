1db0f234d699e719cdf2e65c76a871e6c1123d24
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-15 14:47:07 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,6 +4,11 @@ require "pry"
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
+  # projects: kickstarter.css('li.project.grid_4')
+  # title: project.css('h2.bbcard_name strong a').text
+  # image link: project.css('div.project-thumbnail a img').attribute('src').value
+  # description: project.css('p.bbcard_blurb').text
+  # location: project.css('ul.project-meta span.location-name').first.text
   binding.pry
 end
 
